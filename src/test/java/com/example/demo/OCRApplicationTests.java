f22cf8ed647973f18d8e16cf1ad0602c1b2c5ceb
littleBearQvQ <2539878540@qq.com>
2022-03-30 10:51:31 +0800

    ocr

---
+++ src/test/java/com/example/demo/OCRApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.example.demo.ocr.OcrUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class OCRApplicationTests {
+
+
+    @Test
+    public void ExcelExportTest() throws Exception {
+        /*System.out.println(OcrUtils.orcCn("D:\\Works\\demo\\orcTest.jpg"));*/
+        System.out.println(OcrUtils.orcCn("D:\\Works\\demo\\test.png"));
+
+    }
+
+}
+
+
